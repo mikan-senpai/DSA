1b38b28537fcbcd826c0e148c8c5806f78ea5556
mikan <santanud6d@gmail.com>
2021-10-18 20:14:04 +0530

    binary searcch algo

--- kunal dsa bootcamp java/binarySearch/src/com/company/binarySearch.java
+++ kunal dsa bootcamp java/binarySearch/src/com/company/binarySearch.java
@@ -3,6 +3,46 @@ package com.company;
 public class binarySearch {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr={-18,-14,-12,0,2,3,4,12,18,19,45,89};
+        int target=113;
+        int ans = binarysearch(arr,target);
+        System.out.println(ans);
     }
+
+    //return the index
+    static int binarysearch(int[] arr,int target )
+    {
+        int start=0;
+        int end= arr.length-1;
+//        int index=-1;
+
+        while(start<=end)
+        {
+
+//      finding the middle element
+//      int middle= (start + end )/2;
+//      this is the better way to find miidle element
+            int middle = start + (end-start) /   2;
+
+            if(arr[middle]<target)
+            {
+                start=middle+1;
+            }
+            else if(arr[middle]>target)
+            {
+                end=middle-1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+
+        return -1;
+
+
+    }
+
+
+
 }
