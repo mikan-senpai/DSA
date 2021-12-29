11d639e070f55fb3c756ad741bab98ef47c461b9
mikan <santanud6d@gmail.com>
2021-12-29 21:21:12 +0530

    maze updated

--- kunal dsa bootcamp java/recursion/src/backtracking/Maze.java
+++ kunal dsa bootcamp java/recursion/src/backtracking/Maze.java
@@ -7,6 +7,13 @@ public class Maze {
 		System.out.println(count(3, 3));
 		path("", 3, 3);
 		System.out.println(pathRet("",3,3));
+
+		boolean board[][]={
+				{true,true,true},
+				{true,false,true},
+				{true,true,true}
+		};
+		pathResistance(" ",board,0,0);
 	}
 
 	static int count(int row, int col) {
@@ -49,5 +56,52 @@ public class Maze {
 		}
 		return ans;
 	}
+
+
+
+//	if we can go diagonally in the maze
+
+    static ArrayList<String> pathDiagonal(String p,int row, int col){
+        if (row == 1 && col == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        if (row > 1 && col > 1){
+            
+        }
+        if (row > 1) {
+            ans.addAll(pathDiagonal(p + "V", row - 1, col));
+        }
+        if (col > 1) {
+            ans.addAll(pathDiagonal(p + "H", row, col - 1));
+        }
+        return ans;
+    }
+
+
+	// MAZE WITH OBSTACLES
+
+	static void pathResistance(String p,boolean maze[][], int row, int col) {
+		if (row == maze.length-1 && col == maze[0].length-1) {
+			System.out.println(p);
+			return;
+		}
+		if(!maze[row][col]){
+			return;
+		}
+		if (row < maze.length-1) {
+			pathResistance(p + "D", maze,row + 1, col);
+		}
+		if (col <maze[0].length-1) {
+			pathResistance(p + "R", maze,row, col + 1);
+		}
+	}
+
+
+
+
+
 }
 
